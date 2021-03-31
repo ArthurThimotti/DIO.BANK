149a962a237538ebb50885d7a8a109f4bcb3c4e9
ArthurThimotti <clear>
2021-03-31 12:56:16 -0300

    Projeto Iniciado

---
+++ DIO.BANK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO.BANK
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
